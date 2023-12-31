587d238f1c07d3565a83f258c2b7b09d4f4bab0a
Bawantha <bawanthapasqual@gmail.com>
2023-12-31 12:29:53 +0530

    hello

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
-const dotEnv = require("dotenv")
+const cors = require('cors');
+const dotEnv = require('dotenv');
 
-dotEnv.config()
+// Load environment variables from .env file
+dotEnv.config();
 
+// Set default values for PORT and HOST
 const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || "44.208.32.102"
+const HOST = process.env.HOST || '44.208.32.102';
+
 app.use(express.json());
 app.use(cors());
 
-const db = require("./models");
+const db = require('./models');
 
 // Routers
-const routes = require("./routes/index.routes");
-app.use("/", routes);
+const routes = require('./routes/index.routes');
+app.use('/', routes);
 
 try {
-
-    db.Admissions.belongsTo(db.Patients, { as: "patients", foreignKey: "PatientId", onDelete: 'cascade'});
-    db.Patients.hasMany(db.Admissions, { as: "admissions", foreignKey: "PatientId"});
-    db.VitalSigns.belongsTo(db.Patients, { as: "patients", foreignKey: "PatientId"});
-    db.Patients.hasMany(db.VitalSigns, { as: "vital_signs", foreignKey: "PatientId"});
-    db.Users.belongsTo(db.UserRoles, { as: "roles", foreignKey: "roleId"});
-    db.UserRoles.hasMany(db.Users, {as: "users", foreignKey: "roleId"});
-    db.Admissions.belongsTo(db.Beds, {as: "bed", foreignKey: "bedId"});
-    db.Beds.hasMany(db.Admissions, {as: "admissions", foreignKey: "bedId"});
-
+  db.Admissions.belongsTo(db.Patients, {
+    as: 'patients',
+    foreignKey: 'PatientId',
+    onDelete: 'cascade',
+  });
+  db.Patients.hasMany(db.Admissions, {
+    as: 'admissions',
+    foreignKey: 'PatientId',
+  });
+  db.VitalSigns.belongsTo(db.Patients, {
+    as: 'patients',
+    foreignKey: 'PatientId',
+  });
+  db.Patients.hasMany(db.VitalSigns, {
+    as: 'vital_signs',
+    foreignKey: 'PatientId',
+  });
+  db.Users.belongsTo(db.UserRoles, {
+    as: 'roles',
+    foreignKey: 'roleId',
+  });
+  db.UserRoles.hasMany(db.Users, { as: 'users', foreignKey: 'roleId' });
+  db.Admissions.belongsTo(db.Beds, { as: 'bed', foreignKey: 'bedId' });
+  db.Beds.hasMany(db.Admissions, { as: 'admissions', foreignKey: 'bedId' });
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
+// Start the server after syncing the database
 db.sequelize.sync({ alter: true }).then(() => {
-    app.listen(3002, () => {
-        console.log("SERVER RUNNING ON PORT 3001");
-    });
-
-    app.listen(PORT,HOST,() => console.log(`Server running on ${HOST} at ${PORT}`));
-})
-
+  app.listen(PORT, HOST, () => {
+    console.log(`Server running on ${HOST}:${PORT}`);
+  });
+});
